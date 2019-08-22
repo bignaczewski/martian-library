cfa022f51625674583acb2f655338b4da0360a97
Bartosz Ignaczewski <bartosz.ignaczewski@sumcumo.com>
2019-08-22 09:59:40 +0200

    Rubocop updates

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class GraphqlController < ApplicationController
   def execute
     variables = ensure_hash(params[:variables])
@@ -9,35 +11,37 @@ class GraphqlController < ApplicationController
     }
     result = MartianLibrarySchema.execute(query, variables: variables, context: context, operation_name: operation_name)
     render json: result
-  rescue => e
+  rescue StandardError => e
     raise e unless Rails.env.development?
+
     handle_error_in_development e
   end
 
   private
 
-  # Handle form data, JSON body, or a blank value
-  def ensure_hash(ambiguous_param)
-    case ambiguous_param
-    when String
-      if ambiguous_param.present?
-        ensure_hash(JSON.parse(ambiguous_param))
-      else
+    # Handle form data, JSON body, or a blank value
+    def ensure_hash(ambiguous_param)
+      case ambiguous_param
+      when String
+        if ambiguous_param.present?
+          ensure_hash(JSON.parse(ambiguous_param))
+        else
+          {}
+        end
+      when Hash, ActionController::Parameters
+        ambiguous_param
+      when nil
         {}
+      else
+        raise ArgumentError, "Unexpected parameter: #{ambiguous_param}"
       end
-    when Hash, ActionController::Parameters
-      ambiguous_param
-    when nil
-      {}
-    else
-      raise ArgumentError, "Unexpected parameter: #{ambiguous_param}"
     end
-  end
 
-  def handle_error_in_development(e)
-    logger.error e.message
-    logger.error e.backtrace.join("\n")
+    def handle_error_in_development(error)
+      logger.error error.message
+      logger.error error.backtrace.join("\n")
 
-    render json: { error: { message: e.message, backtrace: e.backtrace }, data: {} }, status: 500
-  end
+      render json:   { error: { message: error.message, backtrace: error.backtrace }, data: {} },
+             status: :internal_server_error
+    end
 end
