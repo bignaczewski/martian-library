d07a4082f5164f9746e0945bd1650519cda6aeab
Bartosz Ignaczewski <bartosz.p.ignaczewski@gmail.com>
2019-08-24 14:24:32 +0200

    Item model validations

--- app/graphql/mutations/update_item_mutation.rb
+++ app/graphql/mutations/update_item_mutation.rb
@@ -8,7 +8,7 @@ module Mutations
     argument :image_url, String, required: false
 
     field :item, Types::ItemType, null: true
-    field :errors, [String], null: false
+    field :errors, Types::ValidationErrorsType, null: true
 
     def resolve(id:, title:, description: nil, image_url: nil)
       check_authentication!
@@ -18,7 +18,7 @@ module Mutations
       if item.update(title: title, description: description, image_url: image_url)
         { item: item }
       else
-        { errors: item.errors.full_messages }
+        { errors: item.errors }
       end
     end
   end
